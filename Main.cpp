a24e8f00c047f6f780cd0d400263031469284fc5
晨魔 <139463472+MorningDemon515@users.noreply.github.com>
2025-01-27 15:21:36 +0800

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -14,6 +14,7 @@ Camera camera(glm::vec3(0.0f, 0.0f, 3.0f), glm::vec3(0.0f, 1.0f, 0.0f), -90.0f, 
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 
 int main()
 {
@@ -38,8 +39,11 @@ int main()
 
     //stbi_set_flip_vertically_on_load(true);
 
-    ShaderFromFile* shader = new ShaderFromFile("vertex.txt","fragment.txt");
-    Model* m = new Model("model/173_2.b3d");
+    ShaderFromFile* shader = new ShaderFromFile("glsl/vertex.txt","glsl/fragment.txt");
+    ShaderFromFile* shader2 = new ShaderFromFile("glsl/vertex.txt","glsl/fragment.txt");
+    Model* m = new Model("model/yinlang.fbx");
+
+    Model* m2 = new Model("model/173.fbx");
 
     while (run)
     {
@@ -84,13 +88,22 @@ int main()
                 camera.processMouseMovement(event.motion.xrel, -event.motion.yrel);
         }
 
-        glClearColor(1.0f,0.0f,0.0f,1.0f);
+        glClearColor(0.0f,0.0f,0.0f,1.0f);
         glClear(GL_COLOR_BUFFER_BIT| GL_DEPTH_BUFFER_BIT);
+
         glEnable(GL_DEPTH_TEST);
         glEnable(GL_BLEND);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
         shader->Use();
+
+        glUniform3f(shader->GetShaderSourceUniform("light.direction"), -0.2f, -1.0f, -0.3f);
+        glUniform3fv(shader->GetShaderSourceUniform("viewPos"),1,&camera.getPosition()[0]);
+
+        // light properties
+        glUniform3f(shader->GetShaderSourceUniform("light.ambient"), 0.2f, 0.2f, 0.2f);
+        glUniform3f(shader->GetShaderSourceUniform("light.diffuse"), 0.5f, 0.5f, 0.5f);
+        glUniform3f(shader->GetShaderSourceUniform("light.specular"), 1.0f, 1.0f, 1.0f);
 
         glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
         glm::mat4 view = camera.getViewMatrix();
@@ -99,22 +112,40 @@ int main()
         
         glm::mat4 model = glm::mat4(1.0f);
         model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f)); 
+        model = glm::rotate(model,glm::radians(360.0f),glm::vec3(1.0f,0.0f,0.0f));
         model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	
         glUniformMatrix4fv(shader->GetShaderSourceUniform("model"),1,GL_FALSE,&model[0][0]);
         
         m->Draw(*shader);
 
-        glm::vec3 position = camera.getPosition();
-        glm::vec3 front = camera.getFront();
+        shader2->Use();
 
-        std::cout << "Camera Position: (" << position.x << ", " << position.y << ", " << position.z << ")\n";
-        std::cout << "Camera Front: (" << front.x << ", " << front.y << ", " << front.z << ")\n";
+        glUniform3f(shader2->GetShaderSourceUniform("light.direction"), -0.2f, -1.0f, -0.3f);
+        glUniform3fv(shader2->GetShaderSourceUniform("viewPos"),1,&camera.getPosition()[0]);
+
+        // light properties
+        glUniform3f(shader2->GetShaderSourceUniform("light.ambient"), 0.2f, 0.2f, 0.2f);
+        glUniform3f(shader2->GetShaderSourceUniform("light.diffuse"), 0.5f, 0.5f, 0.5f);
+        glUniform3f(shader2->GetShaderSourceUniform("light.specular"), 1.0f, 1.0f, 1.0f);
+
+        glUniformMatrix4fv(shader2->GetShaderSourceUniform("projection"),1,GL_FALSE,&projection[0][0]);
+        glUniformMatrix4fv(shader2->GetShaderSourceUniform("view"),1,GL_FALSE,&view[0][0]);
+        
+        model = glm::translate(model, glm::vec3(0.0f, 0.0f, 10.0f)); 
+        model = glm::rotate(model,glm::radians(270.0f),glm::vec3(1.0f,0.0f,0.0f));
+        model = glm::rotate(model,glm::radians(180.0f),glm::vec3(0.0f,0.0f,1.0f));
+        model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	
+        glUniformMatrix4fv(shader2->GetShaderSourceUniform("model"),1,GL_FALSE,&model[0][0]);
+        
+        m2->Draw(*shader2);
 
         SDL_GL_SwapWindow(window);
     }
     
     delete shader;
+    delete shader2;
     delete m;
+
     SDL_GL_DeleteContext(glc);
     SDL_DestroyWindow(window);
     SDL_Quit();
